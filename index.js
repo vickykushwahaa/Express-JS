538bc20a8f3adba5c071d694a6f422e0a354adbb
Vicky Kumar Kushwaha <vickykumarkusgwaha@gmail.com>
2025-04-12 16:29:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,29 +7,29 @@ app.listen(port, () => {
     console.log(`Server is running on ${port}`);
 });
 
-// app.use((req, res) =>{
-//    let code = "<h1>hello</h1>"
-//     res.send(code);
-// });
+app.use((req, res) =>{
+   let code = "<h1>hello</h1>"
+    res.send(code);
+});
 
 
-// create get method
+//create get method
 
-// app.get("/",(req,res)=>{
-//   res.send("you are in home page")
-// });
-// app.get("/about",(req,res)=>{
-//   res.send("you are in about page")
-// });
+app.get("/",(req,res)=>{
+  res.send("you are in home page")
+});
+app.get("/about",(req,res)=>{
+  res.send("you are in about page")
+});
 
-// app.get("/contact",(req,res)=>{
-//   res.send("you are in contact page")
-// });
+app.get("/contact",(req,res)=>{
+  res.send("you are in contact page")
+});
 
-// app.get("/:username",(req,res)=>{
-//   console.log(req.params);
-//   res.send(`hello ${req.params.username}`);
-// });
+app.get("/:username",(req,res)=>{
+  console.log(req.params);
+  res.send(`hello ${req.params.username}`);
+});
 
 app.get("/search",(req,res)=>{
   console.log(req.query);
